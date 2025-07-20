8a0117fcce08b399954fc55d033dfe4c01121f10
lacerdaaa <edulacerdaaa@gmail.com>
2025-07-19 22:19:39 -0300

    feat(letmeask): configure fastify server, cors and enviroment variables with zod validation

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import { fastify } from 'fastify';
+import {
+    type ZodTypeProvider,
+    validatorCompiler,
+    serializerCompiler,
+} from 'fastify-type-provider-zod';
+import { fastifyCors } from '@fastify/cors';
+import { env } from './env.ts';
+
+const app = fastify().withTypeProvider<ZodTypeProvider>();
+
+app.register(fastifyCors, {
+    origin: 'http://localhost:5173',
+});
+
+app.setSerializerCompiler(serializerCompiler),
+    app.setValidatorCompiler(validatorCompiler);
+
+app.listen({ port: env.PORT }).then(() => console.log(`HTTP Server running on port ${env.PORT}.`))
